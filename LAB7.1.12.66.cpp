fb6211485abf9ebd4c179ed95b6e346cd074b7b2
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2023-12-07 15:17:54 +0700

    Update LAB7.1.12.66.cpp

--- LAB7.1.12.66.cpp
+++ LAB7.1.12.66.cpp
@@ -16,11 +16,10 @@ int main()
     string ch;
 
     cin >> ch;
-    if (ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U" || ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U") {
+    if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u" || ch == "A" || ch == "E" || ch == "I" || ch == "O" || ch == "U") {
         cout << "Vowel" << endl;
     } else {
         cout << "Consonant" << endl;
     }
     return 0;
 }
-
